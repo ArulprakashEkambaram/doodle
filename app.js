e02f5f4996da691c6701dd1979589991cdb06369
ArulprakashEkambaram <arulprakash.ekam@gmail.com>
2017-07-24 15:31:27 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+var app = angular.module('myApp', []);
+app.controller('TestCtrl',function($scope, $http ) {
+     $http.get('test.json').success(function(response) {
+        $scope.myTestJson = response;
+       // console.log(JSON.stringify(response));
+
+      $scope.add = function (){
+        alert("add is called");
+        //$scope.myTestJson.push($scope.jsonNames);
+        $scope.myTestJson.push($scope.myTestJson, jsonNames);
+      };
+       $scope.delete = function (index){
+        $scope.myTestJson.splice(index,1);
+        alert("JSON Name is deleted");
+      }
+     $scope.saveUpdate = function (index) {
+            $scope.myTestJson[index] = $scope.dynamiceditedModel;
+            $scope.edited = -1;
+        };
+        //$scope.dynamiceditedModel=$scope.myTestJson;
+    });
+  });
